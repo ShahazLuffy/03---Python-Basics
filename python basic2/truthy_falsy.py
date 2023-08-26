1d7a5274bc4fe75412c31079f10f9cdf148d5e66
shahaz <ali.ghaderipour@gmail.com>
2023-08-26 13:23:37 +0330

    123

---
+++ python basic2/truthy_falsy.py
@@ -0,0 +1,14 @@
+"""
+falsy values in Python include:
+
+    The number zero (0)
+    An empty string ('')
+    False
+    None
+    An empty list ([])
+    An empty tuple (())
+    An empty dictionary ({})
+    An empty set (set())
+
+
+"""
---
+++ ternary.py
@@ -0,0 +1,7 @@
+user_score = 90
+print("Next level") if user_score > 50 else print("Repeat level")
+
+
+to_check = 5
+msg = "Odd" if to_check%2  else "Even"
+print(msg) 
