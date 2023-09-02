3b26a15b4c579cb0a44bf350cbc2bf5e6a7f0fee
shahaz <ali.ghaderipour@gmail.com>
2023-09-02 06:48:54 +0330

    123

--- 3-OOP/5- class_method_and_static_method.py
+++ 3-OOP/5- class_method_and_static_method.py
@@ -1,7 +1,7 @@
 class PlayerCharacter: 
     membership = True 
 # we learned that we can have attribute for a class 
-# but what about a method
+# but what about a method? we want method for class
     def __init__(self, name, age): 
         
         if(age > 18):
@@ -10,3 +10,9 @@ class PlayerCharacter:
     def shout(self): 
         print(f'my name is {self.name}') 
       
+    @classmethod
+    def sum_numbers(num1,num2):
+        return num1+num2
+
+player1= PlayerCharacter('karim',22)
+player1.sum_numbers(2)
