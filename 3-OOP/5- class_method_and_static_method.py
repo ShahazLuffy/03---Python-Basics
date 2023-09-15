1eaca170176ff9911d006d4b75ff17657396989f
Shahaz <ali.ghaderipour@gmail.com>
2023-09-15 16:31:41 +0330

    140206241631

--- 3-OOP/5- class_method_and_static_method.py
+++ 3-OOP/5- class_method_and_static_method.py
@@ -9,4 +9,24 @@ class PlayerCharacter:
             self.age = age
     def shout(self): 
         print(f'my name is {self.name}') 
-      
+    
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return num1 + num2
+    @classmethod
+    def instanciate_classmethod(cls,num1, num2):
+        return cls('teddy', num1+num2)
+    @staticmethod # same as classmethod except no access to cls
+    def test():
+        pass
+    
+player1 = PlayerCharacter('Tom' , 20)
+print(player1.adding_things(2,3))
+
+# we do not need to instanciate when we need class method
+print(PlayerCharacter.adding_things(4,5))
+
+# we can use cls to instanciate
+player3 = PlayerCharacter.instanciate_classmethod(5,26)
+print(player3.name)
+print(player3.age)
