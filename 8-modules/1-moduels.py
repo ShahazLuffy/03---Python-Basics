452f9578daf7d07bf8c67b13ac30fe988e447003
Shahaz <ali.ghaderipour@gmail.com>
2023-09-18 22:13:11 +0330

    14020628213

---
+++ 8-modules/1-moduels.py
@@ -0,0 +1,3 @@
+# some .py files which helps us for organizing our code
+# for example we are going to create utility.py files and use it all our project
+# this utilit.py may have some functions, classes, etc.
